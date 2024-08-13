388669ecfc8eb649308b93811cb0f401f45fea1d
SAFOU Sarah <safou.sarah.m@gmail.com>
2024-08-13 14:41:14 +0100

    clean: separation of projects data from the code

---
+++ src/data/dataProjects.js
@@ -0,0 +1,77 @@
+const dataProjects = {
+  en: [
+    {
+      id: 1,
+      title: 'Authentication App With React',
+      description: 'How to Build a Full-Stack Authentication App with React',
+      link: '#',
+      style: 'full',
+    },
+    {
+      id: 2,
+      title: 'Dynamic CV',
+      description: 'Generate dynamic pdf CV with React',
+      link: '#',
+      style: 'empty',
+    },
+    {
+      id: 3,
+      title: 'Documents & images store',
+      description: 'Documents & images store with React and Django',
+      link: '#',
+      style: 'full',
+    },
+    {
+      id: 4,
+      title: 'Documents & images store',
+      description: 'Documents & images store with React and Django',
+      link: '#',
+      style: 'empty',
+    },
+    {
+      id: 5,
+      title: 'Newsletter App',
+      description: 'Create & send custom newsletter with react & django',
+      link: '#',
+      style: 'full',
+    },
+  ],
+  fr: [
+    {
+      id: 1,
+      title: 'Authentication App With React',
+      description: 'How to Build a Full-Stack Authentication App with React',
+      link: '#',
+      style: 'full',
+    },
+    {
+      id: 2,
+      title: 'Dynamic CV',
+      description: 'Generate dynamic pdf CV with React',
+      link: '#',
+      style: 'empty',
+    },
+    {
+      id: 3,
+      title: 'Documents & images store',
+      description: 'Documents & images store with React and Django',
+      link: '#',
+      style: 'full',
+    },
+    {
+      id: 4,
+      title: 'Documents & images store',
+      description: 'Documents & images store with React and Django',
+      link: '#',
+      style: 'empty',
+    },
+    {
+      id: 5,
+      title: 'Newsletter App',
+      description: 'Create & send custom newsletter with react & django',
+      link: '#',
+      style: 'full',
+    },
+  ],
+}
+export default dataProjects
--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -1,49 +1,34 @@
 import { useTranslation } from 'react-i18next'
+import dataProjects from '../data/dataProjects'
 
 function Projects() {
-  const { t } = useTranslation()
+  const { t, i18n } = useTranslation()
+  const language = i18n.language
+  const projects = dataProjects[language] || []
   return (
     <section id='projects'>
-      <div class='container'>
-        <div class='header'>
-          <span class='big-title'>{t('projects_title')}</span>
+      <div className='container'>
+        <div className='header'>
+          <span className='big-title'>{t('projects_title')}</span>
           <p>{t('projects_description')}</p>
           <br />
         </div>
-        <div class='content'>
-          <div>
-            <div class='card full'>
-              <span class='title'>Authentication App With React</span>
-              <p>How to Build a Full-Stack Authentication App with React</p>
-              <span class='link'>{t('go_to_project')}</span>
-            </div>
-            <div class='card empty'>
-              <span class='title'>Dynamic CV</span>
-              <p>Generate dynamic pdf CV with React</p>
-              <span class='link'>{t('go_to_project')}</span>
-            </div>
-            <div class='card full'>
-              <span class='title'>Documents & images store</span>
-              <p>Documents & images store with React and Django</p>
-              <span class='link'>{t('go_to_project')}</span>
-            </div>
-          </div>
-          <div>
-            <div class='card empty'>
-              <span class='title'>Documents & images store</span>
-              <p>Documents & images store with React and Java</p>
-              <span class='link'>{t('go_to_project')}</span>
-            </div>
-            <div class='card full'>
-              <span class='title'>Newsletter App</span>
-              <p>Create & send custom newsletter with react & django</p>
-              <span class='link'>{t('go_to_project')}</span>
-            </div>
-            <div class='card empty final'>
-              <div class='text'>{t('more_projects')}</div>
-              <div>
-                <i class='fa-solid fa-hand-pointer'></i>
+        <div className='content'>
+          {projects.map((project) => {
+            return (
+              <div key={project.id} className={`card ${project.style}`}>
+                <span className='title'>{project.title}</span>
+                <p>{project.description}</p>
+                <span className='link' href={project.link}>
+                  {t('go_to_project')}
+                </span>
               </div>
+            )
+          })}
+          <div className='card empty final'>
+            <div className='text'>{t('more_projects')}</div>
+            <div>
+              <i className='fa-solid fa-hand-pointer'></i>
             </div>
           </div>
         </div>
