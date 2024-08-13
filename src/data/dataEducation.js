b7c844ed0dfa27f4cf65d117babf989cb91ecd21
SAFOU Sarah <safou.sarah.m@gmail.com>
2024-08-13 15:13:41 +0100

    clean: separation of educations data from the code

---
+++ src/data/dataEducation.js
@@ -0,0 +1,77 @@
+const dataEducation = {
+  en: [
+    {
+      id: 1,
+      date: 'AOUT 2018 / NOVEMBER 2018',
+      primaryTitle: 'Formation en ligne, ',
+      secondaryTitle: 'ONE MILLION ARABCODERS ',
+      description: 'Front-End Developer track',
+      awardsLink: '#',
+    },
+    {
+      id: 2,
+      date: 'SEPTEMBRE 2016 / JUIN 2018',
+      primaryTitle: 'Master en ingénierie Logiciels, ',
+      secondaryTitle:
+        'Univesité Des Sciences et de LA Technologie Houari Boumedienne USTHB',
+      description: '',
+      awardsLink: '',
+    },
+    {
+      id: 3,
+      date: 'SEPTEMBRE 2012 / JUIN 2016',
+      primaryTitle:
+        'Licence en Ingénierie des Systèmes d’Information et Logiciels, ',
+      secondaryTitle:
+        'Univesité Des Sciences et de LA Technologie Houari Boumedienne USTHB',
+      description: '',
+      awardsLink: '',
+    },
+    {
+      id: 4,
+      date: 'JUIN 2012',
+      primaryTitle: 'Baccalauréat série sciences expérimentales, ',
+      secondaryTitle: 'Lycée MOHEMMED LABJAOUI',
+      description: 'Mention : Bien',
+      awardsLink: '',
+    },
+  ],
+  fr: [
+    {
+      id: 1,
+      date: 'AOUT 2018 / NOVEMBER 2018',
+      primaryTitle: 'Formation en ligne, ',
+      secondaryTitle: 'ONE MILLION ARABCODERS ',
+      description: 'Front-End Developer track',
+      awardsLink: '#',
+    },
+    {
+      id: 2,
+      date: 'SEPTEMBRE 2016 / JUIN 2018',
+      primaryTitle: 'Master en ingénierie Logiciels, ',
+      secondaryTitle:
+        'Univesité Des Sciences et de LA Technologie Houari Boumedienne USTHB',
+      description: '',
+      awardsLink: '',
+    },
+    {
+      id: 3,
+      date: 'SEPTEMBRE 2012 / JUIN 2016',
+      primaryTitle:
+        'Licence en Ingénierie des Systèmes d’Information et Logiciels, ',
+      secondaryTitle:
+        'Univesité Des Sciences et de LA Technologie Houari Boumedienne USTHB',
+      description: '',
+      awardsLink: '',
+    },
+    {
+      id: 4,
+      date: 'JUIN 2012',
+      primaryTitle: 'Baccalauréat série sciences expérimentales, ',
+      secondaryTitle: 'Lycée MOHEMMED LABJAOUI',
+      description: 'Mention : Bien',
+      awardsLink: '',
+    },
+  ],
+}
+export default dataEducation
