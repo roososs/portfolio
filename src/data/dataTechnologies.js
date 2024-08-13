4ac0b5ec97fda13144f2a9fcc111996142a2fe51
SAFOU Sarah <safou.sarah.m@gmail.com>
2024-08-13 16:24:42 +0100

    fet: add translation to data Tachnologies

--- src/data/dataTechnologies.js
+++ src/data/dataTechnologies.js
@@ -1,63 +1,127 @@
-const dataTechnologies = [
-  {
-    tooltip:
-      'Angular - A popular front-end framework for building web applications.',
-    icon: 'fa-angular',
-  },
-  {
-    tooltip:
-      'HTML5 - The latest version of the HyperText Markup Language used for structuring web pages.',
-    icon: 'fa-html5',
-  },
-  {
-    tooltip:
-      'CSS3 - The latest version of the Cascading Style Sheets used for styling web pages.',
-    icon: 'fa-css3-alt',
-  },
-  {
-    tooltip:
-      'Java - A versatile programming language used for building platform-independent applications.',
-    icon: 'fa-java',
-  },
-  {
-    tooltip:
-      'JavaScript - A programming language used to create interactive effects within web browsers.',
-    icon: 'fa-js-square',
-  },
-  {
-    tooltip:
-      'PHP - A server-side scripting language used for web development and creating dynamic web pages.',
-    icon: 'fa-php',
-  },
-  {
-    tooltip:
-      'React - A JavaScript library for building user interfaces, especially single-page applications.',
-    icon: 'fa-react',
-  },
-  {
-    tooltip:
-      'Figma - A collaborative interface design tool used for creating and prototyping user interfaces.',
-    icon: 'fa-figma',
-  },
-  {
-    tooltip:
-      'Git - A version control system for tracking changes in source code during software development.',
-    icon: 'fa-git-alt',
-  },
-  {
-    tooltip:
-      'GitHub - A platform for version control and collaboration using Git, hosting code repositories.',
-    icon: 'fa-github',
-  },
-  {
-    tooltip:
-      'GitLab - A DevOps platform providing Git repository management, CI/CD pipelines, and more.',
-    icon: 'fa-gitlab',
-  },
-  {
-    tooltip:
-      'Docker - A platform for developing, shipping, and running applications in containers.',
-    icon: 'fa-docker',
-  },
-]
+const dataTechnologies = {
+  en: [
+    {
+      tooltip:
+        'Angular - A popular front-end framework for building web applications.',
+      icon: 'fa-angular',
+    },
+    {
+      tooltip:
+        'HTML5 - The latest version of the HyperText Markup Language used for structuring web pages.',
+      icon: 'fa-html5',
+    },
+    {
+      tooltip:
+        'CSS3 - The latest version of the Cascading Style Sheets used for styling web pages.',
+      icon: 'fa-css3-alt',
+    },
+    {
+      tooltip:
+        'Java - A versatile programming language used for building platform-independent applications.',
+      icon: 'fa-java',
+    },
+    {
+      tooltip:
+        'JavaScript - A programming language used to create interactive effects within web browsers.',
+      icon: 'fa-js-square',
+    },
+    {
+      tooltip:
+        'PHP - A server-side scripting language used for web development and creating dynamic web pages.',
+      icon: 'fa-php',
+    },
+    {
+      tooltip:
+        'React - A JavaScript library for building user interfaces, especially single-page applications.',
+      icon: 'fa-react',
+    },
+    {
+      tooltip:
+        'Figma - A collaborative interface design tool used for creating and prototyping user interfaces.',
+      icon: 'fa-figma',
+    },
+    {
+      tooltip:
+        'Git - A version control system for tracking changes in source code during software development.',
+      icon: 'fa-git-alt',
+    },
+    {
+      tooltip:
+        'GitHub - A platform for version control and collaboration using Git, hosting code repositories.',
+      icon: 'fa-github',
+    },
+    {
+      tooltip:
+        'GitLab - A DevOps platform providing Git repository management, CI/CD pipelines, and more.',
+      icon: 'fa-gitlab',
+    },
+    {
+      tooltip:
+        'Docker - A platform for developing, shipping, and running applications in containers.',
+      icon: 'fa-docker',
+    },
+  ],
+  fr: [
+    {
+      tooltip:
+        "Angular - Un framework front-end populaire pour la création d'applications web.",
+      icon: 'fa-angular',
+    },
+    {
+      tooltip:
+        'HTML5 - La dernière version du langage HyperText Markup pour structurer les pages web.',
+      icon: 'fa-html5',
+    },
+    {
+      tooltip:
+        'CSS3 - La dernière version des feuilles de style en cascade pour styliser les pages web.',
+      icon: 'fa-css3-alt',
+    },
+    {
+      tooltip:
+        'Java - Un langage de programmation polyvalent utilisé pour créer des applications indépendantes de la plateforme.',
+      icon: 'fa-java',
+    },
+    {
+      tooltip:
+        'JavaScript - Un langage de programmation utilisé pour créer des effets interactifs dans les navigateurs web.',
+      icon: 'fa-js-square',
+    },
+    {
+      tooltip:
+        'PHP - Un langage de script côté serveur utilisé pour le développement web et la création de pages web dynamiques.',
+      icon: 'fa-php',
+    },
+    {
+      tooltip:
+        "React - Une bibliothèque JavaScript pour la création d'interfaces utilisateur, notamment pour les applications à page unique.",
+      icon: 'fa-react',
+    },
+    {
+      tooltip:
+        "Figma - Un outil de conception d'interfaces collaboratif utilisé pour créer et prototyper des interfaces utilisateur.",
+      icon: 'fa-figma',
+    },
+    {
+      tooltip:
+        'Git - Un système de contrôle de version pour suivre les modifications du code source pendant le développement logiciel.',
+      icon: 'fa-git-alt',
+    },
+    {
+      tooltip:
+        'GitHub - Une plateforme pour le contrôle de version et la collaboration utilisant Git, hébergeant des dépôts de code.',
+      icon: 'fa-github',
+    },
+    {
+      tooltip:
+        'GitLab - Une plateforme DevOps offrant la gestion de dépôts Git, des pipelines CI/CD, et plus encore.',
+      icon: 'fa-gitlab',
+    },
+    {
+      tooltip:
+        'Docker - Une plateforme pour développer, expédier et exécuter des applications dans des conteneurs.',
+      icon: 'fa-docker',
+    },
+  ],
+}
 export default dataTechnologies
